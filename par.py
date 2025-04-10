bcd1d5bc59e66b2b78693e1295144bcbfaf691d0
Diego Serra <diego.serra@gmail.com>
2025-04-10 20:53:35 -0300

    primera version de programa evaluador de numero par

---
+++ par.py
@@ -0,0 +1,8 @@
+def main():
+    numero = int(input("Ingrese numero positivo para conocer su paridad: "))
+    if (numero%2 == 0):
+        print("El numero es par")
+    else:
+        print("El numero no par")
+
+main()
