f4e12e490a6fa11d766a1c5485ef0118479112ae
Diego Serra <diego.serra@gmail.com>
2025-04-10 21:00:54 -0300

    nueva funcion es_par

--- par.py
+++ par.py
@@ -1,6 +1,9 @@
+def es_par(num):
+    return num%2 == 0
+
 def main():
     numero = int(input("Ingrese numero positivo para conocer su paridad: "))
-    if (numero%2 == 0):
+    if (es_par(numero)):
         print("El numero es par")
     else:
         print("El numero no par")
